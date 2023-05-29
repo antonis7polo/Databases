6dfc385385eef792a745faf2be630cceb349eaaf
harrypapa2002 <75332013+harrypapa2002@users.noreply.github.com>
2023-05-29 19:35:40 +0300

    Update indexes.sql

--- SQL_Code/indexes.sql
+++ SQL_Code/indexes.sql
@@ -1,8 +1,12 @@
 CREATE INDEX idx_rental_date ON Rental(Rental_Date);
+CREATE INDEX idx_return_date ON Rental(Return_Date);
 CREATE INDEX idx_title ON Book(Title);
 CREATE INDEX idx_author ON Author(First_Name,Last_Name);
 CREATE INDEX idx_available_copies ON School_Books(Available_Copies);
 CREATE UNIQUE INDEX idx_genre ON Category(Genre);
+CREATE UNIQUE INDEX idx_username ON User(Username);
+
+
 
 
 
